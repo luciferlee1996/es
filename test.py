1ac3033db7005f39849c8392e975697071d629e8
LuC Lee <LuC Lee>
2016-10-24 12:02:04 +0800

    1st

---
+++ test.py
@@ -0,0 +1,56 @@
+import urllib.request
+file_name = input("which file?")
+file = open(file_name, "r")
+print("file " + file_name + " opened")
+
+'''
+term_list = file.read().replace('\n', ' ').replace(',', ' ').replace.replace('?', ' ').replace(';', ' ').split()
+'''
+
+term_list = file.read()
+
+for c in ['\n', ',', '.', '?', '!', ';', '\'s']:
+    if c in term_list:
+        term_list = term_list.replace(c, ' ')
+
+str_german = 'German'
+str_latin = 'Latin'
+
+res = { 'german' : 0,
+        'latin' : 0
+      }
+left = []
+
+total = len(term_list)
+cnt = 0
+
+for term in term_list:
+    cnt = cnt + 1
+    print("processing {0}/{1}".format(cnt, total))
+    url = 'http://etymonline.com/index.php?term=' + term + '&allowed_in_frame=0'
+    page = urllib.request.urlopen(url)
+    page_content = page.read().decode('iso-8859-1')
+    #print(type(page_content))
+    #page_byte = bytes(page_content, encoding = "utf8")
+    have_ger = False
+    have_lat = False
+    if str_german in page_content:
+        res['german'] += 1
+        have_ger = True
+    if str_latin in page_content:
+        res['latin'] += 1
+        have_lat = True
+    if not have_ger and not have_lat:
+        left.append(term)
+    
+'''
+print(term_list)
+'''
+
+print(term)
+print("\ngerman:{0};\nlatin:{1}".format(res['german'], res['latin']))
+
+
+file.close()
+print("file " + file_name + " closed")
+
